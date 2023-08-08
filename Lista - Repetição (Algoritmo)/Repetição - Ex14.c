4ca1d0bd1f414ff3f7d400ebabe4964db717d4a9
ThiagoOyo <107907441+ThiagoOyo@users.noreply.github.com>
2023-08-07 21:45:30 -0300

    Update and rename Repetição - Ex14.cpp to Repetição - Ex14.c

--- Lista - Repetição (Algoritmo)/Repetição - Ex14.c
+++ Lista - Repetição (Algoritmo)/Repetição - Ex14.c
@@ -7,7 +7,7 @@ int main(){
 	setlocale(LC_ALL, "Portuguese_Brazil");
 	float numero,hn=0.0;
     int i=0;
-    printf("Forneça o numero: ");
+    printf("ForneÃ§a o numero: ");
     scanf("%f",&numero);
     for ( i = 1; i <= numero; i++){
         hn += 1.0/i;
